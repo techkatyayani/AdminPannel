b91b3e343e3fc6687a528f00fe024cd740a3a771
praveen soni <ece.praveen2003@gmail.com>
2024-04-15 22:25:56 +0530

    init third

--- lib/Screens/Banners.dart
+++ lib/Screens/Banners.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:image_picker/image_picker.dart';
+
 
 class BannerScreen extends StatefulWidget {
   const BannerScreen({Key? key}) : super(key: key);
@@ -8,8 +12,10 @@ class BannerScreen extends StatefulWidget {
   State<BannerScreen> createState() => _BannerScreenState();
 }
 
+
 class _BannerScreenState extends State<BannerScreen> {
   late Future<Map<String, dynamic>> userDataFuture;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
@@ -30,66 +36,160 @@ class _BannerScreenState extends State<BannerScreen> {
     }
   }
 
+  Future<void> _pickImageAndUpdate(String imageName) async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      File imageFile = File(pickedFile.path);
+
+      String imageUrl = await _uploadImage(imageFile, imageName);
+
+      await updateImageUrlInFirestore(imageName, imageUrl);
+
+      setState(() {
+        userDataFuture = getImage();
+      });
+    }
+  }
+
+  Future<String> _uploadImage(File imageFile, String imageName) async {
+    try {
+      firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child('images')
+          .child(imageName);
+      await ref.putFile(imageFile);
+      return await ref.getDownloadURL();
+    } catch (e) {
+      print("Error uploading image: $e");
+      return '';
+    }
+  }
+
+  Future<void> updateImageUrlInFirestore(String imageName, String imageUrl) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('imagefromfirebase')
+          .doc('Category')
+          .update({imageName: imageUrl});
+    } catch (e) {
+      print("Error updating image URL in Firestore: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Banner Screen'),
-      ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: userDataFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
             Map<String, dynamic> userData = snapshot.data ?? {};
             return Container(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade200
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: width,
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(color: Colors.blue),
+                      child: Text('Hindi Slide Banner', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: width * 0.01),),
                     ),
-                    child: Text('Hindi Slide Banner\'s'),
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image(
-                        image: NetworkImage(userData['150 product hindi.jpg'] ?? '',),
-                        height:100,
-                        width: 200,
-                      ),
-                      Image(
-                        image: NetworkImage(userData['best selling hindi.jpg'] ?? '',),
-                        height:100,
-                        width: 200,
-                      ),
-                      Image(
-                        image: NetworkImage(userData['agri advisor hindi.jpg'] ?? '',),
-                        height:100,
-                        width: 200,
-                      ),
-                      Image(
-                        image: NetworkImage(userData['crop calender hindi.jpg'] ?? '',),
-                        height:100,
-                        width: 200,
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            );
+                    SizedBox(height: 20,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () => _pickImageAndUpdate('Demo.jpg'),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2.0,
+                              ),
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.network(
+                                userData['Demo.jpg'] ?? '',
+                                width: width * 0.2,
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        GestureDetector(
+                          onTap: () => _pickImageAndUpdate('best selling hindi.jpg'),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2.0,
+                              ),
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.network(
+                                userData['best selling hindi.jpg'] ?? '',
+                                width: width * 0.2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => _pickImageAndUpdate('agri advisor hindi.jpg'),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2.0,
+                              ),
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.network(
+                                userData['agri advisor hindi.jpg'] ?? '',
+                                width: width * 0.2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () => _pickImageAndUpdate('crop calender hindi.jpg'),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2.0,
+                              ),
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.network(
+                                userData['crop calender hindi.jpg'] ?? '',
+                                width: width * 0.2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ));
           }
         },
       ),
     );
   }
 }
+

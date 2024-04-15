b5b6096a48cf3922965d35e8e68a10138917615c
praveen soni <ece.praveen2003@gmail.com>
2024-04-15 15:55:25 +0530

    init second

--- lib/Screens/Banners.dart
+++ lib/Screens/Banners.dart
@@ -1,16 +1,95 @@
-
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BannerScreen extends StatefulWidget {
-  const BannerScreen({super.key});
+  const BannerScreen({Key? key}) : super(key: key);
 
   @override
   State<BannerScreen> createState() => _BannerScreenState();
 }
 
 class _BannerScreenState extends State<BannerScreen> {
+  late Future<Map<String, dynamic>> userDataFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    userDataFuture = getImage();
+  }
+
+  Future<Map<String, dynamic>> getImage() async {
+    final DocumentReference user1 = FirebaseFirestore.instance
+        .collection('imagefromfirebase')
+        .doc('Category');
+    try {
+      DocumentSnapshot userSnapshot = await user1.get();
+      return userSnapshot.data() as Map<String, dynamic>;
+    } catch (e) {
+      print(e.toString());
+      return {};
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Banner Screen'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: userDataFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            Map<String, dynamic> userData = snapshot.data ?? {};
+            return Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade200
+                    ),
+                    child: Text('Hindi Slide Banner\'s'),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image(
+                        image: NetworkImage(userData['150 product hindi.jpg'] ?? '',),
+                        height:100,
+                        width: 200,
+                      ),
+                      Image(
+                        image: NetworkImage(userData['best selling hindi.jpg'] ?? '',),
+                        height:100,
+                        width: 200,
+                      ),
+                      Image(
+                        image: NetworkImage(userData['agri advisor hindi.jpg'] ?? '',),
+                        height:100,
+                        width: 200,
+                      ),
+                      Image(
+                        image: NetworkImage(userData['crop calender hindi.jpg'] ?? '',),
+                        height:100,
+                        width: 200,
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            );
+          }
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,13 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.web,
   );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,6 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
